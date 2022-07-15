12d3520835784d627c6e8894c0a24da108ee4f65
Francis KOUAHO <kouahofrancis@gmail.com>
2022-07-15 17:08:58 +0200

    add data project

--- src/components/Projets.js
+++ src/components/Projets.js
@@ -1,6 +1,49 @@
 import "../assets/styles/Projets.css"
+import {stockData} from "../data";
+/*import {useEffect, useRef} from "react";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+import {gsap} from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);*/
 
 const Projets = () => {
+
+    /*const AnimateScrollingProjet = useRef(null);
+
+    useEffect(() => {
+        gsap.to(AnimateScrollingProjet.current, {
+            scrollTrigger: {
+                scrub: 1,
+                trigger: AnimateScrollingProjet.current,
+                start: "-35% top",
+                onEnter: () => console.log('Enter'),
+                onLeave: () => console.log('Leave'),
+                onEnterBack: () => console.log('Enter back'),
+                onLeaveBack: () => console.log('All the way back'),
+                //markers: "true"
+            },
+            bottom: 500
+        })
+    })
+
+    const AnimateScrollingProjet_2 = useRef(null);
+
+    useEffect(() => {
+        gsap.to(AnimateScrollingProjet_2.current, {
+            scrollTrigger: {
+                scrub: 1,
+                trigger: AnimateScrollingProjet_2.current,
+                start: "-130% top",
+                onEnter: () => console.log('Enter'),
+                onLeave: () => console.log('Leave'),
+                onEnterBack: () => console.log('Enter back'),
+                onLeaveBack: () => console.log('All the way back'),
+                markers: "true"
+            },
+            bottom: 1000
+        })
+    })*/
+
     return (
         <section id="cases" className="">
             {/*<div className="bggrid bg">
@@ -11,38 +54,27 @@ const Projets = () => {
                 <div></div>
             </div>*/}
             <div className="container">
-                <article>
-                    <a href="case/frl" title="Facebook Reality Labs" draggable="false"
-                       data-text="Facebook Reality Labs">
+                {
+                    stockData.map((data, index) => {
+                        return (
+                            <article key={index}>
+                                <a href="case/frl" title="Facebook Reality Labs" draggable="false"
+                                   data-text="Facebook Reality Labs">
                         <span>
-                            <span>Aux</span>
+                            <span>{data.companyUn}</span>
                             <span>&#x202F;</span>
-                            <span>Fins</span>
                         </span>
-                        <span>
-                            <span>Gastronomes</span>
+                                    <span>
+                            <span>{data.companyTwo}</span>
                         </span>
-                    </a>
-                    <div className="info">
-                        <div className="separator"></div>
-                        <p>Conception d’un site pour la commecerlisation de produit Traiteur.</p></div>
-                </article>
-                <article>
-                    <a href="case/frl" title="Facebook Reality Labs" draggable="false"
-                       data-text="Facebook Reality Labs">
-                        <span>
-                            <span>Aux</span>
-                            <span>&#x202F;</span>
-                            <span>Fins</span>
-                        </span>
-                        <span>
-                            <span>Gastronomes</span>
-                        </span>
-                    </a>
-                    <div className="info">
-                        <div className="separator"></div>
-                        <p>Conception d’un site pour la commecerlisation de produit Traiteur.</p></div>
-                </article>
+                                </a>
+                                <div className="info">
+                                    <div className="separator"></div>
+                                    <p>Conception d’un site pour la commecerlisation de produit Traiteur.</p></div>
+                            </article>
+                        )
+                    })
+                }
             </div>
         </section>
     )
---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const stockData = [
+    {
+        companyUn: "Aux fins",
+        companyTwo: "Gastronomes",
+    },
+    {
+        companyUn: "Sud renovation",
+        companyTwo: "Piscine",
+    },
+    {
+        companyUn: "Messiant",
+        companyTwo: "Calot",
+    },
+    {
+        companyUn: "Francis",
+        companyTwo: "KOUAHO",
+    },
+    {
+        companyUn: "Let'sGO",
+        companyTwo: "",
+    },
+    {
+        companyUn: "Let'sGO",
+        companyTwo: "",
+    },
+];
