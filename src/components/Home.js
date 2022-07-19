28c74b121aff40f23291927e1e40807beec754ac
Francis KOUAHO <kouahofrancis@gmail.com>
2022-07-19 19:46:54 +0200

    delete remove && merge home and project

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,84 @@
 import {ScrollPage} from "react-scroll-motion";
 import "../assets/styles/Home.css"
-import Projets from "./Projets";
+import "../assets/styles/Projets.css"
 import {useEffect, useRef} from "react";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
 import {gsap} from "gsap";
+import {stockData} from "../config/data";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Home = () => {
-
-    const homeRef = useRef(null)
     const sectionHome = useRef(null)
+    const revealRefs = useRef([])
+
+    revealRefs.current = []
+
+    const addToRefs = (item) => {
+        if (item && !revealRefs.current.includes(item)) {
+            revealRefs.current.push(item)
+        }
+        console.log(revealRefs.current)
+    }
+
+
+
     useEffect(() => {
-        gsap.to(homeRef.current, {
-            ScrollTrigger: {
-                trigger: homeRef.current,
-                pin: sectionHome.current,
-                start: 'top top',
-                endtrigger: '.circle-block',
-                //end: 'bottom top-=' + window.innerHeight,
-                end: () => 'bottom top' + window.innerHeight,
-                marker: true
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".layer-story-pinned-content",
+                scrub: true,
+                pin: true,
+                start: "top 30",
+                end: '3000px',
+                //endTrigger: '.layer-story-pinned-content',
+                markers: true
+            }
+        });
+
+        tl.from(
+            ".layer-story-content-wrapper",
+            { y: 20, autoAlpha: 0, stagger: 0.5 },
+            0
+        ).to(
+            ".layer-story-content-wrapper",
+            {
+                y: 0,
+                autoAlpha: 0,
+                stagger: 0.5
             },
-            x: "-100px - 50%",
+            0.4
+        );
+        tl.from(
+            ".layer-story-image",
+            { y: 0, autoAlpha: 0, stagger: 0.5 },
+            0
+        ).to(
+            ".layer-story-image",
+            {
+                y: 0,
+                autoAlpha: 1,
+                stagger: 0.5
+            },
+            0.4
+        );
+
+        revealRefs.current.forEach((item, index) => {
+            gsap.to(item, {
+                scrollTrigger: {
+                    scrub: 1,
+                    id: `${index + 1}`,
+                    start: "top 100%",
+                    end: "bottom 0%",
+                    trigger: item,
+                    toggleActions: "play none none reverse",
+                    //markers: true
+                },
+                top: 20,
+                paused: true,
+                reversed: true,
+                yPercent: 0
+            })
         })
 
     })
@@ -30,7 +86,38 @@ const Home = () => {
     return (
         <ScrollPage page={1}>
             <section className="section-home" ref={sectionHome}>
-                <Projets/>
+                <section id="cases">
+                    {/*<div className="bggrid bg">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>*/}
+                    <div className="container">
+                        {
+                            stockData.map(({index, companyUn, companyTwo}) => {
+                                return (
+                                    <article key={index} ref={addToRefs}>
+                                        <a href="case/frl" title="Facebook Reality Labs" draggable="false"
+                                           data-text="Facebook Reality Labs">
+                        <span>
+                            <span>{companyUn}</span>
+                            <span>&#x202F;</span>
+                        </span>
+                                            <span>
+                            <span>{companyTwo}</span>
+                        </span>
+                                        </a>
+                                        <div className="info">
+                                            <div className="separator"></div>
+                                            <p>Conception d’un site pour la commecerlisation de produit Traiteur.</p></div>
+                                    </article>
+                                )
+                            })
+                        }
+                    </div>
+                </section>
             </section>
         </ScrollPage>
     )
