2597d42cafec3103b909bd5b5ab7261330d14236
FrancisKOUAHO <kouahofrancis@gmail.com>
2023-08-23 20:36:05 +0200

    chatbot

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,54 +6,12 @@ import Skills from "../components/Skills/Skills";
 import Projects from "../components/Projects/Projects";
 import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
+import Script from 'next/script'
 import {useEffect, useRef} from "react";
 import gsap from 'gsap';
 
 const Home: NextPage = () => {
-    const blobRef = useRef<HTMLDivElement>(null);
     const containerRef = useRef(null);
-
-    useEffect(() => {
-        const blob = blobRef.current;
-
-        if (!blob) {
-            return;
-        }
-
-        const blobRect = blob.getBoundingClientRect();
-        const blobSizes = {
-            width: blobRect.width,
-            height: blobRect.height
-        };
-
-        const move = (event: MouseEvent) => {
-            let mouseX = event.clientX - blobSizes.width / 2;
-            let mouseY = event.clientY - blobSizes.height / 2;
-
-            if (raf) {
-                cancelAnimationFrame(raf);
-            }
-            raf = requestAnimationFrame(() => {
-                blob.style.setProperty('--mouse-x', mouseX + 'px');
-                blob.style.setProperty('--mouse-y', mouseY + 'px');
-            });
-        };
-
-        const resize = () => {
-            const rect = blob.getBoundingClientRect();
-            blobSizes.width = rect.width;
-            blobSizes.height = rect.height;
-        };
-
-        let raf: number | null = null;
-        window.addEventListener("mousemove", move);
-        window.addEventListener("resize", resize);
-
-        return () => {
-            window.removeEventListener("mousemove", move);
-            window.removeEventListener("resize", resize);
-        };
-    }, []);
 
     useEffect(() => {
         const tl = gsap.timeline({defaults: {duration: 1}});
@@ -99,7 +57,6 @@ const Home: NextPage = () => {
           <div className="min-h-full relative fadeup-enter-header">
               <TheHeader/>
           </div>
-          <div className="blob" ref={blobRef}></div>
           <div
             className="relative flex justify-center sm:justify-between w-full items-center gap-6 sm:gap-12 flex-col-reverse sm:flex-row Profile_profileHeight__1Q9Zk fadeup-enter-profile">
               <Presentation/>
@@ -111,6 +68,7 @@ const Home: NextPage = () => {
           <div className="flex flex-col gap-12 mt-16 sm:mt-0 sm:gap-24 items-center fadeup-enter-projects">
               <Projects/>
           </div>
+          <Script src="https://connect.myaichat.io/index.js"/>
           <div className="flex flex-col gap-12 mt-16 sm:mt-0 sm:gap-24 items-center fadeup-enter-contact">
               <Contact/>
           </div>
